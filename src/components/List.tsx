28832ce14eba5ef73f53fe600d14789b32d97520
Josmanuel Sandrea <josmanueljsg@gmail.com>
2023-02-08 18:51:41 -0500

    Modal in progress

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,12 +2,13 @@ import Task from "./Task"
 import { ContextTodo } from '../utils/ContextTodo'
 import { useContext } from "react"
 import { InfinitySpin } from "react-loader-spinner"
+import { PlusCircleIcon } from "@heroicons/react/24/solid"
 
 const List = () => {
     const { active_list, loading, currentTasks } = useContext(ContextTodo)
 
     return (
-        <div className="flex flex-col w-full justify-center flex-auto">
+        <div className="flex flex-col w-full justify-center items-center flex-auto relative">
             <ul className="flex justify-center flex-col items-center gap-5">
                 <div>
                     {loading && <InfinitySpin color="#1E3A8A"/>}
@@ -20,6 +21,9 @@ const List = () => {
                     })}
                 </div>
             </ul>
+            <div className="absolute right-5 bottom-5 w-[4em]">
+                <PlusCircleIcon className="text-blue-900"/>
+            </div>
         </div>
     )
 }
--- src/components/ListMaster.tsx
+++ src/components/ListMaster.tsx
@@ -1,5 +1,6 @@
-import { ContextProvider} from '../utils/ContextTodo'
+import { ContextProvider } from '../utils/ContextTodo'
 import { ListDataType } from '../utils/ContextTodo'
+import { PlusIcon } from '@heroicons/react/24/solid'
 import NavigationText from './NavigationText'
 import List from './List'
 
@@ -8,10 +9,31 @@ const ListMaster = () => {
         <ContextProvider>
             <div className='flex flex-auto font-Ubuntu flex-col'>
                 <div className='mt-5 flex justify-center w-full gap-5 h-fit mb-5'>
-                    <NavigationText text={ListDataType.Completed}/>
-                    <NavigationText text={ListDataType.Uncompleted}/>
+                    <NavigationText text={ListDataType.Completed} />
+                    <NavigationText text={ListDataType.Uncompleted} />
                 </div>
                 <List />
+                <div className='fixed inset-0 flex w-full h-screen justify-center items-center z-50 bg-white'>
+                    <div className="absolute rounded-sm bg-white shadow-sm flex w-[90%] h-[calc(100%-2rem)] border-2 font-Ubuntu flex-col">
+                        <div className='mt-4 flex-initial flex justify-center'>
+                            <h1 className='text-blue-900 text-4xl'>Create new task</h1>
+                        </div>
+                        <div className='flex-auto flex justify-evenly items-center flex-col'>
+                            <div className='w-[70%] flex justify-center flex-col'>
+                                <div>
+                                    <p className='text-2xl'>Task name:</p>
+                                </div>
+                                <input type="text" className='rounded-md border-solid border-2 text-2xl px-2 py-2 border-blue-200 focus-visible:outline-none'/>
+                            </div>
+                            <button className='rounded-md flex items-center text-2xl border-2 px-2 py-1 border-blue-200 hover:text-white hover:bg-blue-900'>
+                                <span>Add task</span>
+                                <div className='w-[1.3em]'>
+                                    <PlusIcon />
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </ContextProvider>
     )
